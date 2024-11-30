5cfee4d4a64c49989569132343ef57f4af94599d
hamzeh010 <hamzehsalhi92@gmail.com>
2024-11-30 05:11:29 +0300

    Fix isLoggedIn in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -39,7 +39,7 @@ const Header: React.FC<HeaderProps> = ({
   useEffect(() => {
     const loggedInStatus = isLoggedIn();
     setLoggedIn(loggedInStatus);
-  }, []);
+  }, [isLoggedIn()]);
 
   const handleLogout = async () => {
     try {
