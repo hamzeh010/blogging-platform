4ff8b21704ce2d4907f6544dfe16aebc79535795
hamzeh010 <hamzehsalhi92@gmail.com>
2024-11-30 19:46:00 +0300

    Change text-center to text-left in details page

--- components/BlogDetails.tsx
+++ components/BlogDetails.tsx
@@ -16,7 +16,7 @@ const BlogDetails: React.FC = () => {
   return (
     <section className="bg-white dark:bg-gray-900 h-full">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+        <div className="mx-auto max-w-screen-sm text-left lg:mb-16 mb-8">
           {post ? (
             <>
               <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
