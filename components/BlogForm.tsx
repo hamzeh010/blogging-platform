faf11c3c9ebe86ec79af8831edad9e179fe72a47
hamzeh010 <hamzehsalhi92@gmail.com>
2024-11-30 19:38:37 +0300

    Change maxLength for Details textarea

--- components/BlogForm.tsx
+++ components/BlogForm.tsx
@@ -170,9 +170,9 @@ const BlogForm: React.FC = () => {
                                     name="details"
                                     value={article.details}
                                     onChange={handleChange}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-[200px]"
                                     placeholder="Enter description"
-                                    maxLength={250}
+                                    maxLength={800}
                                 />
                             </div>
                             <Button variant="primary" size="medium" type="submit"  className="w-full" >
